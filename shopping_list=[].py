42100a5f87e7dffeb202e3039d53711753a683c8
Adeljaber0 <adeljaber200510@gmail.com>
2025-09-28 11:22:37 +0300

    Add files via upload

---
+++ shopping_list=[].py
@@ -0,0 +1,48 @@
+shopping_list=[]
+
+def add_list():
+    item =input("ُEnter the item name to add:")
+    if item.strip()==" ":
+        print ("It is not allowed to enter without value")
+        return
+    if len(item)< 3 :
+        print("Enter an element that contains at least three letters")
+        return
+    
+    if "product" not in item:
+        print("Enter an element that contains Product keyword")
+        return 
+
+    shopping_list.append(item)
+    print (f"{item} In addition to the shopping menu")
+  
+def show_list():
+    print ("list items:")
+    for x in shopping_list:
+        print (x)
+
+def delete_item():
+    item=input("Enter the item name to delete:")
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"'{item}'It was successfully deleted")    
+    else:
+        print (f"'{item}'The element does not exist")
+     
+def exit_item():
+    print ("exit pro")
+ 
+    user= int (input("enter the opertion 1.add - 2.show  -3.delete:"))
+
+    if user==1:
+        add_list()
+
+    elif user==2:
+        show_list()
+
+    elif user==3:
+        delete_item()
+                
+    else:
+        print("erore")
+        
